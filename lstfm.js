3ef4da8f6bc70eb0781f30efd8b45758991a3b4f
Adel Rahimi <rahimi.adel@gmail.com>
2016-03-28 18:06:23 +0430

    update

--- lstfm.js
+++ lstfm.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=defsamm&api_key=5b801a66d1a34e73b6e563afc27ef06b&limit=2&format=json&callback=?", function(data) {
+    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=prof_hustler&api_key=2f796f77b5018ac3b9f0e10d308ef5f8&limit=2&format=json&callback=?", function(data) {
 
         var html = ''; // we declare the variable that we'll be using to store our information
         var counter = 1; // we declare a counter variable to use with the if statement in order to limit the result to 1
@@ -10,7 +10,7 @@ $(document).ready(function() {
             } // close the if statement
             counter++ // add 1 to the counter variable each time the each loop runs
         }); // close each loop
-        $('.listening-to h5').append(html); // print the information to the document - here I look for the h5 tag inside the div with a class of 'listening-to' and use the jQuery append method to insert the information we've stored in the html variable inside the h5 tag.
+        $('list').append(html); // print the information to the document - here I look for the h5 tag inside the div with a class of 'listening-to' and use the jQuery append method to insert the information we've stored in the html variable inside the h5 tag.
     }); // close JSON call
 
 }); // close document ready function
