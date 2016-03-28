4b5b697270ce8eab6c4a7dd89e562b41d94c54c0
Adel Rahimi <rahimi.adel@gmail.com>
2016-03-28 23:55:44 +0430

    up

---
+++ _includes/lstfm.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=prof_hustler&api_key=2f796f77b5018ac3b9f0e10d308ef5f8&limit=5&format=json&callback=?", function(data) {
+      var html = ''; // we declare the variable that we'll be using to store our information
+      var counter = 1; // we declare a counter variable to use with the if statement in order to limit the result to 1
+      $.each(data.recenttracks.track, function(i, item) {
+          if(counter == 1) {
+              html += 'Currently listening to: <span><a href="' + item.url + '" target="_blank">' + item.name + '</a> - ' + item.artist['#text'] + '</span>';
+          } // close the if statement
+          counter++ // add 1 to the counter variable each time the each loop runs
+      }); // close each loop
+      $('#list').append(html); // print the information to the document.
+    });
+});
